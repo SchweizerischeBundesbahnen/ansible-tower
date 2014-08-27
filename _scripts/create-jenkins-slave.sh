d892812f7deecdde5802d51d39e12f7b0473235b
Masen Igor <igor.masen2@sbb.ch>
2014-08-27 11:20:06 +0200

    WZU-2510 mehrere Bugfixes, run.sh zum Ausführen des Jenkins - alle Checks grün

--- _scripts/create-jenkins-slave.sh
+++ _scripts/create-jenkins-slave.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 externalfshome=/var/lib/docker/container-ext-filesystems
-randomint=$RANDOM
+randomint=`shuf -i 50000-60000 -n1`
 
 # create container
 sudo mkdir -p $externalfshome/$randomint/mavenrepo $externalfshome/$randomint/tmp $externalfshome/$randomint/workspace
 sudo chown -R 1090:1090 $externalfshome/$randomint
-sudo docker run -d -m 12g -v "$externalfshome/$randomint/mavenrepo:/var/data/jenkins/m2/repository" -v "$externalfshome/$randomint/tmp:/tmp" -v "$externalfshome/$randomint/workspace:/var/data/jenkins/workspace" -e master=https://ci.sbb.ch -e executors=3 -name jenkins-slave-jee-$randomint jenkins-slave-jee
+sudo docker run -p $randomint:22 -d -m 10g -v "$externalfshome/$randomint/mavenrepo:/var/data/jenkins/m2/repository" -v "$externalfshome/$randomint/tmp:/tmp" -v "$externalfshome/$randomint/workspace:/var/data/jenkins/workspace" -e master=https://ci.sbb.ch -e executors=3 -e ciuser=fsvctip -e cipassword=sommer11 --name jenkins-slave-jee-$randomint jenkins-slave-jee
--- _scripts/replace-all-slaves.sh
+++ _scripts/replace-all-slaves.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
-allcontainers=`sudo docker ps --all | grep -v "CONTAINER" | cut -d" " -f1`
+allcontainers=`sudo docker ps | grep -v "CONTAINER" | cut -d" " -f1`
 
 for container in $allcontainers; do ./remove-jenkins-slave.sh $container; ./create-jenkins-slave.sh; done
