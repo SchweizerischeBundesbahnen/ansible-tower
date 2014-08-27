d5ef992a805a145e967ff8a4466131fc5a9dbafc
Masen Igor <igor.masen2@sbb.ch>
2014-08-27 13:58:33 +0200

    WZU-2510 final version

--- _scripts/replace-all-slaves.sh
+++ _scripts/replace-all-slaves.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
-allcontainers=`sudo docker ps | grep -v "CONTAINER" | cut -d" " -f1`
+allcontainers=`sudo docker ps | grep -v "CONTAINER" | grep -v c9c71 | grep -v d989 | grep -v e1f3e | grep -v e5f6b | cut -d" " -f1`
 
 for container in $allcontainers; do ./remove-jenkins-slave.sh $container; ./create-jenkins-slave.sh; done
