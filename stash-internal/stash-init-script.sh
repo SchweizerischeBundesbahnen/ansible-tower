513f606e35af808a19e9c9192a87d2457e867c16
u217229 <sebastian.graf@sbb.ch>
2014-11-18 15:48:38 +0100

    WZU-2627: working on init script

--- stash-internal/stash-init-script.sh
+++ stash-internal/stash-init-script.sh
@@ -4,7 +4,7 @@
 #
 # chkconfig: - 85 15
 
-opts=" --privileged=true -p 8090:8090 -v /var/data/stash/log:/var/data/atlassian-base/log -v /var/data/stash/logs:/var/data/atlassian-base/logs -v /var/data/stash/shared/data:/var/data/atlassian-base/shared/data -d -m 10g"
+opts=" --privileged=true -p 8090:8090 -v /var/data/stash/log:/var/data/stash/log -v /var/data/stash/logs:/var/data/stash/logs -v /var/data/stash/shared/data:/var/data/stash/shared/data -d"
 containername=stash
 imagename=schweizerischebundesbahnen/stash-internal:3.4.1
 
