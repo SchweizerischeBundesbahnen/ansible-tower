d293abf0b8f02caaca965b0ae7dd5b8b76aa86d3
Christian Eichenberger <u214892@sbb.ch>
2014-12-19 09:47:32 +0100

    WZU-2663 Encapsulated WMB-Builds

--- _scripts/create-jenkins-wmb-slave.sh
+++ _scripts/create-jenkins-wmb-slave.sh
@@ -5,4 +5,4 @@ randomint=`shuf -i 50000-60000 -n1`
 # create container
 sudo mkdir -p $externalfshome/$randomint/mavenrepo $externalfshome/$randomint/tmp $externalfshome/$randomint/workspace
 sudo chown -R 1091:1091 $externalfshome/$randomint
-sudo docker run -p $randomint:22 -d -v "$externalfshome/$randomint/mavenrepo:/var/data/jenkins/m2/repository" -v "$externalfshome/$randomint/tmp:/tmp" -v "$externalfshome/$randomint/workspace:/var/data/jenkins/workspace" -e master=http://ci-t.sbb.ch -e executors=1 -e ciuser=fsvctip -e cipassword=sommer11 -e labels=yveyvess --name jenkins-slave-wmb-$randomint schweizerischebundesbahnen/jenkins-slave-wmb
+sudo docker run -p $randomint:22 -d -v "$externalfshome/$randomint/mavenrepo:/var/data/jenkins/m2/repository" -v "$externalfshome/$randomint/tmp:/tmp" -v "$externalfshome/$randomint/workspace:/var/data/jenkins/workspace" -e master=http://ci-t.sbb.ch -e executors=1 -e ciuser=fsvctip -e cipassword=sommer11 -e labels=wmb --name jenkins-slave-wmb-$randomint schweizerischebundesbahnen/jenkins-slave-wmb
