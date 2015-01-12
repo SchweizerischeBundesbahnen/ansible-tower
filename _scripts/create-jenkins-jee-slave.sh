035679f4dea7eded3d9c6499887ac1195de57699
Igor Masen <igor.masen2@sbb.ch>
2015-01-12 18:55:40 +0100

    update

--- _scripts/create-jenkins-jee-slave.sh
+++ _scripts/create-jenkins-jee-slave.sh
@@ -5,4 +5,4 @@ randomint=`shuf -i 50000-60000 -n1`
 # create container
 sudo mkdir -p $externalfshome/$randomint/mavenrepo $externalfshome/$randomint/tmp $externalfshome/$randomint/workspace
 sudo chown -R 1091:1091 $externalfshome/$randomint
-sudo docker run -d -v "$externalfshome/$randomint/mavenrepo:/var/data/jenkins/m2/repository" -v "$externalfshome/$randomint/tmp:/tmp" -v "$externalfshome/$randomint/workspace:/var/data/jenkins/workspace" -e master=http://ci-t.sbb.ch -e executors=1 -e ciuser=fsvctip -e cipassword=sommer11 -e labels=jee --name jenkins-slave-jee-$randomint schweizerischebundesbahnen/jenkins-slave-jee
+sudo docker run -d -v "$externalfshome/$randomint/mavenrepo:/var/data/jenkins/m2/repository" -v "$externalfshome/$randomint/tmp:/tmp" -v "$externalfshome/$randomint/workspace:/var/data/jenkins/workspace" -e master=https://ci.sbb.ch -e executors=1 -e ciuser=fsvctip -e cipassword=sommer11 -e labels=jee --name jenkins-slave-jee-$randomint schweizerischebundesbahnen/jenkins-slave-jee
