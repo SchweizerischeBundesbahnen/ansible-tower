1cc3d69021a75b28c16eb0b2ee5e01b64e268397
u214892 <christian.markus.eichenberger@sbb.ch>
2015-01-14 16:57:07 +0100

    WZU-2994 Docker-Pipeline ueber interne Docker-Registry

--- _scripts/build_and_push_preproc.sh
+++ _scripts/build_and_push_preproc.sh
@@ -2,6 +2,9 @@
 
 feature_branch="`git rev-parse --abbrev-ref HEAD`"
 commit_hash="`git rev-parse HEAD`"
+echo "feature_branch=$feature_branch"
+echo "commit_hash=$commit_hash"
+
 
 # for testing...
 feature_branch="refs/heads/feature/WZU-2994"
