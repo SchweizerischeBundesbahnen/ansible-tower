a00d6a43f39515aec091f819d7451417f7d24578
Roger Bobst <roger.bobst@sbb.ch>
2015-01-14 11:34:20 +0100

    WZU-2994: Params for build

--- _scripts/build_and_push.sh
+++ _scripts/build_and_push.sh
@@ -3,7 +3,8 @@
 
 echo "START-PARAMS: $1 - $2"
 
-REGISTRY=registry-t.sbb.ch
+REGISTRY=$1
+TAG=":$2"
 #IMAGELIST=('base' 'jenkins-slave-base' 'jenkins-master' 'jenkins-slave-base' 'jenkins-slave-js' 'jenkins-slave-mobile-android' 'jenkins-slave-jee' 'jenkins-slave-wmb' 'confluence' 'jira-standalone' 'jrebellicenseserver' 'stash-base' 'stash-internal' 'stash-external')
 IMAGELIST=('base' 'jenkins-master')
 
@@ -12,23 +13,23 @@ error=0
 
 for IMAGE in "${IMAGELIST[@]}"
 do
-	sudo docker build --rm  -t schweizerischebundesbahnen/${IMAGE} ./${IMAGE}
+	sudo docker build --rm  -t schweizerischebundesbahnen/${IMAGE}${TAG} ./${IMAGE}
 	if [ $? -ne 0 ]; then
 		echo "BUILD failed! Image=$IMAGE"
 		error=1
 		break;
 	fi
 
-	sudo docker tag "schweizerischebundesbahnen/${IMAGE}" "${REGISTRY}/${IMAGE}"
-	sudo docker push ${REGISTRY}/${IMAGE}
-	sudo docker rmi -f "${REGISTRY}/${IMAGE}"
+	sudo docker tag "schweizerischebundesbahnen/${IMAGE}${TAG}" "${REGISTRY}/${IMAGE}${TAG}"
+	sudo docker push ${REGISTRY}/${IMAGE}${TAG}
+	sudo docker rmi -f "${REGISTRY}/${IMAGE}${TAG}"
 done
 
 if [ $error -eq 0 ]; then
   for IMAGE in "${IMAGELIST[@]}"
   do
-    sudo docker rmi -f "${REGISTRY}/${IMAGE}"
-    sudo docker rmi -f "schweizerischebundesbahnen/${IMAGE}"
+    sudo docker rmi -f "${REGISTRY}/${IMAGE}${TAG}"
+    sudo docker rmi -f "schweizerischebundesbahnen/${IMAGE}${TAG}"
   done
 fi
 
