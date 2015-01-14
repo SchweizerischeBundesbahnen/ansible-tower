07f1d8955cbb791c179a3efb7623800c4dc5c86d
u214892 <christian.markus.eichenberger@sbb.ch>
2015-01-14 17:40:35 +0100

    WZU-2994 Docker-Pipeline ueber interne Docker-Registry

--- _scripts/build_and_push_preproc.sh
+++ _scripts/build_and_push_preproc.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
+branch=$1
+
 commit_hash="`git rev-parse HEAD`"
 echo "commit_hash=$commit_hash"
 
-feature_branch="`git symbolic-ref HEAD`"
-echo "feature_branch=$feature_branch"
-
-
 
 # for testing...
-feature_branch="refs/heads/feature/WZU-2994"
+feature_branch="refs/heads/$branch"
 
 # if we're not on a feature branch...
-if  [[ $feature_branch == *feature* ]]
+if  [[ $feature_branch != *feature* ]]
 then
     python extract_open_pull_request_id.py ${feature_branch} ${commit_hash}
 fi
