ec2323e026f070ca75bf584a790cb78b4d152b84
u214892 <christian.markus.eichenberger@sbb.ch>
2015-01-14 16:50:15 +0100

    WZU-2994 Docker-Pipeline ueber interne Docker-Registry

--- _scripts/build_and_push_preproc.sh
+++ _scripts/build_and_push_preproc.sh
@@ -4,9 +4,14 @@ feature_branch="`git rev-parse --abbrev-ref HEAD`"
 commit_hash="`git rev-parse HEAD`"
 
 # for testing...
-feature_branch="refs/heads/feature/WZU-2994
+feature_branch="refs/heads/feature/WZU-2994"
 
-# if we're on develop or master, we have to extract the open feature branch...
-python extract_open_pull_request_id.py ${feature_branch} ${commit_hash}
+# if we're not on a feature branch...
+#if  [[ $feature_branch != *feature* ]]
+#then
+    python extract_open_pull_request_id.py ${feature_branch} ${commit_hash}
+#fi
+
+
 
 
