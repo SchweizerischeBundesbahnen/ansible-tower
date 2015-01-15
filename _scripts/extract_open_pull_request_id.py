878266f2eb163bac29a7ed30ce939572e5d6f26b
u214892 <christian.markus.eichenberger@sbb.ch>
2015-01-15 09:50:23 +0100

    WZU-2994 Docker-Pipeline ueber interne Docker-Registry

--- _scripts/extract_open_pull_request_id.py
+++ _scripts/extract_open_pull_request_id.py
@@ -1,21 +1,7 @@
 #!/usr/bin/python
 ####################################################################################
-##BLOCK STASH REPOSITORIES
+##Get pull request id for
 ####################################################################################
-#This script blocks the develop branch of a various number of Stash Repositories
-#under a given project.
-####################################################################################
-#Workflow of restricting mavendemo in kd_wzu
-####################################################################################
-#1. GET  https://code.sbb.ch/rest/branch-permissions/1.0/projects/KD_WZU/repos/mavendemo/restricted
-#RETURN: {"size":7,"limit":100,"isLastPage":true,"values":[{"id":222,"type":"BRANCH","value":"refs/heads/develop","branch":{"id":"refs/heads/develop","displayId":"develop","latestChangeset":"fab9a73f5368f4a68ad80fc6025317dc2046ae1d","isDefault":true}},{"id":223,"type":"BRANCH","value":"refs/heads/master","branch":{"id":"refs/heads/master","displayId":"master","latestChangeset":"f11590c20b3397fffc3d6fc27d45a33677033ede","isDefault":false}},{"id":357,"type":"PATTERN","value":"*"},{"id":359,"type":"PATTERN","value":"feature/**"},{"id":358,"type":"PATTERN","value":"hotfix/**"},{"id":361,"type":"PATTERN","value":"release/**"},{"id":360,"type":"PATTERN","value":"tags/**"}],"start":0,"filter":null}
-#ID to Lock: 222
-#2. PUT https://code.sbb.ch/rest/branch-permissions/1.0/projects/KD_WZU/repos/mavendemo/restricted/222
-#CONTENT: {"type": "BRANCH","value": "refs/heads/develop","users": ["release"]}
-####################################################################################
-#USAGE:
-####################################################################################
-#python blocker.py u123456 PASS PROJECT [release|automerge] REPO1 REPO2 REPO3
 ####################################################################################
 
 import httplib
@@ -35,6 +21,8 @@ auth = 'Basic ' + string.strip(base64.encodestring(userid + ':' + passwd))
 #construct the REST URL
 getUrl = '/rest/api/1.0/projects/kd_wzu/repos/wzu-docker/pull-requests?direction=INCOMING&state=open&order=newest&withAttributes=false&withProperties=false&at=' + urllib.quote_plus( feature_branch )
 
+
+
 #getting the permissions for each repos...
 c=httplib.HTTPSConnection(host)
 c.putrequest('GET', getUrl)
@@ -42,7 +30,7 @@ c.putheader('Authorization', auth )
 c.endheaders()
 response = c.getresponse()
 c.close()
-print response.status, response.reason
+#print response.status, response.reason
 data = response.read()
 values = json.loads(data).get("values")
 
@@ -51,8 +39,8 @@ values = json.loads(data).get("values")
 
 #...and find the id of the develop branch
 for value in values:
-    the_id = value["id"]
-
+    if ["latestChangeset"] == commit_hash:
+        the_id = value["id"]
 print the_id
 
 
