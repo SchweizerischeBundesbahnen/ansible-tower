0de94005ed0e51d52435c0cdcc11e70e0d102b59
u214892 <christian.markus.eichenberger@sbb.ch>
2015-01-16 16:51:30 +0100

    WZU-2994 Docker-Pipeline über interne Docker-Registry

--- _scripts/replace-all-nodejs-slaves.sh
+++ _scripts/replace-all-nodejs-slaves.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 allcontainers=`sudo docker ps --all | grep -v "CONTAINER" | grep jenkins-slave-js | cut -d" " -f1`
 
-for container in $allcontainers; do ./remove-jenkins-slave.sh $container; sleep 10; ./create-jenkins-nodejs-slave.sh; done
+for container in $allcontainers; do ./remove-jenkins-slave.sh $container; sleep 10; ./create-jenkins-slave.sh jenkins-slave-js; done
