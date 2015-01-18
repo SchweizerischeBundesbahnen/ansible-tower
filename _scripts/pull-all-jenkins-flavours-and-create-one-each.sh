fd92b24746bd3d142eebb0075c2f517c91765827
Roger Bobst <roger.bobst@sbb.ch>
2015-01-18 22:41:49 +0100

    WZU-2994: new version of deployscript

--- _scripts/pull-all-jenkins-flavours-and-create-one-each.sh
+++ _scripts/pull-all-jenkins-flavours-and-create-one-each.sh
@@ -1,22 +1,41 @@
 #!/bin/bash
 
-tag=$1
-if [[ -z "$tag" ]]; then
-    tag="schweizerischebundesbahnen"
+# takes three arguments:
+# registry: repository to pull the images from (registry-t.sbb.ch)
+# tag: git branch (used as tag) to deploy (feature/WZU-2994)
+# ci to use (http://ci-t.sbb.ch)
+
+echo "Start: $1 - $2 - $3"
+
+# get repository from argument. Defaults to schweizerischebundesbahnen (external registry) if not set.
+repository=$1
+if [[ -z "$repository" ]]; then
+    repository="schweizerischebundesbahnen"
 fi
 
+# get image tag to deploy, since we give the full git branch, we have to remove the feature first
+tag=`basename $2`
+
+# get the master ci
+ci=$3
+
 error=0
-tasklist=('jenkins-slave-base' 'jenkins-slave-js' 'jenkins-slave-mobile-android' 'jenkins-slave-jee' 'jenkins-slave-wmb')
+tasklist=('jenkins-slave-js' 'jenkins-slave-mobile-android' 'jenkins-slave-jee' 'jenkins-slave-wmb')
 
 for task in "${tasklist[@]}"
 do
   echo "$task"
-  sudo docker pull "$tag/$task"
+  sudo docker pull "${repository}/${task}:${tag}"
   if [ $? -ne 0 ]; then
-    echo "PULL failed! Image=$tag/$task"
+    echo "PULL failed! Image=${repository}/${task}:${tag}"
     error=1
     break;
   fi
 done
+
+
+# FIXME: start containers with new images
+
+
 
 exit $error
