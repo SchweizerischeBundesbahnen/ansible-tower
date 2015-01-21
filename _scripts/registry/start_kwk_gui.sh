f2808ecbd17999da75af120d833bc72f8a30ae50
Roger Bobst <roger.bobst@sbb.ch>
2015-01-21 16:29:37 +0100

    WZU-2994: rework of registry shell scripts

--- _scripts/registry/start_kwk_gui.sh
+++ _scripts/registry/start_kwk_gui.sh
@@ -1,6 +1,19 @@
+#!/bin/bash
+#
+# takes the registry to bind to as argument
+#
+if [ $# -eq 0 ]
+  then
+    echo "Usage: start_kwk.sh THE_REGISTRY_URL"
+  exit -1
+fi
+
+REGISTRY=$1
+
 sudo docker run \
   -d \
-  -e ENV_DOCKER_REGISTRY_HOST=registry-t.sbb.ch \
+  -e ENV_DOCKER_REGISTRY_HOST=${REGISTRY} \
   -e ENV_DOCKER_REGISTRY_PORT=5000 \
   -p 8081:80 \
+  --name docker-registry-frontend \
   konradkleine/docker-registry-frontend
