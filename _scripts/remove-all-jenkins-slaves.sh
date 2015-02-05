e463990ea60e72a26079467aa6c931e2276871e0
u210691 <igor.masen@sbb.ch>
2015-02-05 14:19:34 +0100

    WZU-3111 refactored to not accidentally delete not jenkins-slaves containers

--- _scripts/remove-all-jenkins-slaves.sh
+++ _scripts/remove-all-jenkins-slaves.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-allcontainers=`sudo docker ps --all | grep -v "CONTAINER" | cut -d" " -f1`
+allcontainers=`sudo docker ps --all | grep "jenkins-slave" | cut -d" " -f1`
 
 for container in $allcontainers; do ./remove-jenkins-slave.sh $container;   done
