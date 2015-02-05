3a40cdefaa601df6fab110f4f56d06989d96c53b
u210691 <igor.masen@sbb.ch>
2015-02-05 10:26:39 +0100

    WZU-3111 added and improved remove-jenkins-slave.sh

---
+++ _scripts/remove-jenkins-slave.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+containerid=$1
+externalfshome=/var/lib/docker/container-ext-filesystems
+
+if [ ! -z $containerid ]
+then
+	# stop container
+	sudo docker stop $containerid
+	if [ $? -eq 0 ]
+	then 
+
+		# get fs id
+		number=`sudo docker inspect -f='{{.Name}}' ${containerid} | awk -F "-" '{ print $NF }'`
+		# remove container
+		sudo docker rm $containerid
+		# remove fs
+		sudo rm -rf $externalfshome/$number
+	else
+		echo "could not stop container with id ${containerid}"
+		exit 1
+	fi	
+else
+	echo "please provide a containerid"
+fi
