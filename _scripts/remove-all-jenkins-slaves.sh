f6bbdf3dc70c42f57523b59c7f92776a7a6eba78
u210691 <igor.masen@sbb.ch>
2015-02-05 14:34:18 +0100

    WZU-3111 bug fix

--- _scripts/remove-all-jenkins-slaves.sh
+++ _scripts/remove-all-jenkins-slaves.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 allcontainers=`sudo docker ps --all | grep "jenkins-slave" | cut -d" " -f1`
 
-for container in $allcontainers; do ./remove-jenkins-slave.sh $container;   done
+for container in $allcontainers; do ./remove-container.sh $container;   done
