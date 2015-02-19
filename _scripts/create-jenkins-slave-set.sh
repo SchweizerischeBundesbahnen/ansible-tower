8795ce7c11161272747f38136ee96c01dc921e3f
u210691 <igor.masen@sbb.ch>
2015-02-19 12:11:20 +0100

    WZU-3190 script to start a set of slaves made more smart

--- _scripts/create-jenkins-slave-set.sh
+++ _scripts/create-jenkins-slave-set.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+master=$1
 numcpus=`cat /proc/cpuinfo | grep processor | wc -l`
-
-if [ $numcpus -gt 30 ]
+if [ ! -z $master ]
+then
+	if [ $numcpus -gt 30 ]
 	then
-		for i in {1..12}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-jee latest https://ci.sbb.ch jee; done
-		for i in {1..2}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-jee latest https://ci.sbb.ch yves-migration; done
-		for i in {1..2}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-js latest https://ci.sbb.ch nodejs; done
-		for i in {1..4}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-iib9 latest https://ci.sbb.ch iib9; done
-		for i in {1..4}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-mobile-android latest https://ci.sbb.ch android; done
+		for i in {1..12}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-jee latest $master jee; done
+		for i in {1..2}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-jee latest $master yves-migration; done
+		for i in {1..2}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-js latest $master nodejs; done
+		for i in {1..4}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-iib9 latest $master iib9; done
+		for i in {1..4}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-mobile-android latest $master android; done
 	else	
-		for i in {1..1}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-jee latest https://ci.sbb.ch jee; done
-		for i in {1..1}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-jee latest https://ci.sbb.ch yves-migration; done
-		for i in {1..1}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-js latest https://ci.sbb.ch nodejs; done
-		for i in {1..1}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-iib9 latest https://ci.sbb.ch iib9; done
-		for i in {1..1}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-mobile-android latest https://ci.sbb.ch android; done
+		for i in {1..1}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-jee latest $master jee; done
+		for i in {1..1}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-jee latest $master yves-migration; done
+		for i in {1..1}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-js latest $master nodejs; done
+		for i in {1..1}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-iib9 latest $master iib9; done
+		for i in {1..1}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-mobile-android latest $master android; done
+	fi
+else
+	echo "You need to set the master e.g http://ci-t.sbb.ch"
+	exit 1
 fi
