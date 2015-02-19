155e2b2623c13740b4eb2dfae89e13a362b6a7de
u210691 <igor.masen@sbb.ch>
2015-02-19 10:47:41 +0100

    WZU-3190 added script to start a set of jenkins-slaves

---
+++ _scripts/create-jenkins-slave-set.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+numcpus=`cat /proc/cpuinfo | grep processor | wc -l`
+
+if [ $numcpus > 30 ]
+	then
+		for i in {1..12}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-jee latest https://ci.sbb.ch jee; done
+		for i in {1..2}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-jee latest https://ci.sbb.ch yves-migration; done
+		for i in {1..2}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-js latest https://ci.sbb.ch nodejs; done
+		for i in {1..4}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-iib9 latest https://ci.sbb.ch iib9; done
+		for i in {1..4}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-mobile-android latest https://ci.sbb.ch android; done
+	else	
+		for i in {1..1}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-jee latest https://ci.sbb.ch jee; done
+		for i in {1..1}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-jee latest https://ci.sbb.ch yves-migration; done
+		for i in {1..1}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-js latest https://ci.sbb.ch nodejs; done
+		for i in {1..1}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-iib9 latest https://ci.sbb.ch iib9; done
+		for i in {1..1}; do ./create-jenkins-slave.sh registry.sbb.ch jenkins-slave-mobile-android latest https://ci.sbb.ch android; done
+fi
