c6c00b05af4dbfa6dad92d7a30deffdaa2985559
u210691 <igor.masen@sbb.ch>
2015-02-19 11:06:04 +0100

    WZU-3190 added single pull-all-latest-images script

--- _scripts/pull-all-latest-images.sh
+++ _scripts/pull-all-latest-images.sh
@@ -1,3 +1,3 @@
-images=`docker images | grep -v REPOSITORY | cut -d" " -f1 | sort | uniq`
+images=`sudo docker images | grep -v REPOSITORY | cut -d" " -f1 | sort | uniq`
 
 for image in $images; do sudo docker pull $image; done
