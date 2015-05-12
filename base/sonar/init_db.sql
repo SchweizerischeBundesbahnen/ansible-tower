d55396ad3a46fc156e762b0311de99b6a1f3a5c9
u217229 <sebastian.graf@sbb.ch>
2015-05-12 10:44:44 +0200

    WZU-3395: removing sql scripts, is done by wzuscheduler

--- base/sonar/init_db.sql
+++
@@ -1,5 +0,0 @@
-DROP DATABASE sonar;
-CREATE DATABASE sonar CHARACTER SET utf8;
-GRANT ALL PRIVILEGES ON sonar.* TO 'sonar'@'localhost' IDENTIFIED BY 'sonartest';
-GRANT ALL PRIVILEGES ON sonar.* TO 'sonar'@'%' IDENTIFIED BY 'sonartest';
-flush privileges;
--- base/sonar/init_dbJS.sql
+++
@@ -1,5 +0,0 @@
-DROP DATABASE sonarJS;
-CREATE DATABASE sonarJS CHARACTER SET utf8;
-GRANT ALL PRIVILEGES ON sonarJS.* TO 'sonarJS'@'localhost' IDENTIFIED BY 'sonarJStest';
-GRANT ALL PRIVILEGES ON sonarJS.* TO 'sonarJS'@'%' IDENTIFIED BY 'sonarJStest';
-flush privileges;
