4a942f61bd7e0b707bda07bd5834b358894ababb
u214892 <u214892@sbb.ch>
2015-06-18 10:49:07 +0200

    MWESERVICE-5995

--- _scripts/remove-all-dead-containers.sh
+++ _scripts/remove-all-dead-containers.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
 docker ps --all | grep Exited | cut -d " " -f 1 | xargs --no-run-if-empty docker rm
+docker ps --all | grep Dead | cut -d " " -f 1 | xargs --no-run-if-empty docker rm
+
