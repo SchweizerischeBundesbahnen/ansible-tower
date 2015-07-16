b71dff4f25f0acdfabebebda6796e23af8d2c7f7
u214892 <u214892@sbb.ch>
2015-07-16 11:35:57 +0200

    MWESERVICE-6447

---
+++ _scripts/remove-all-jenkins-slaves-by-flavour.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+jenkinsflavour=$1
+if [ -z "$jenkinsflavour" ]; then
+    echo "A Jenkins flavour must be specified: [jenkins-slave-mobile-android,...], see create-jenkins-slave-set.sh"
+fi
+
+allcontainers=`sudo docker ps --all | grep "${jenkinsflavour}" | cut -d" " -f1`
+
+for container in $allcontainers; do ./remove-container.sh $container;   done
--- _scripts/replace-all-jee-slaves.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -x
-allcontainers=`sudo docker ps --all | grep -v "CONTAINER" | grep jenkins-slave-jee | cut -d" " -f1`
-
-for container in $allcontainers; do ./remove-jenkins-slave.sh $container; sleep 10; ./create-jenkins-slave.sh jenkins-slave-jee; done
