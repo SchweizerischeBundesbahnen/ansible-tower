017fed05b3a80cf7e985a753cf4d7d503389c482
Roger Bobst <roger.bobst@sbb.ch>
2015-07-16 15:49:33 +0200

    WZU-3724: Volumes removed and a new name concept for slaves

--- _scripts/create-jenkins-slave.sh
+++ _scripts/create-jenkins-slave.sh
@@ -16,29 +16,27 @@ tag=$3
 master=$4
 labels=$5
 additional_args=$6
-externalfshome=/var/data/docker/container-ext-filesystems
 randomint=`shuf -i 40000-65000 -n1`
 master_hostname=`echo $master | awk -F/ '{print $3}' | awk -F: '{print $1}'`
 
 function check_reserved() {
-	while [ -d $externalfshome/$randomint ]
-	do	
-		randomint=`shuf -i 40000-65000 -n1`
-	done	
-}
 
-function create_directories() {
-	sudo mkdir -p $externalfshome/$randomint/mavenrepo $externalfshome/$randomint/tmp $externalfshome/$randomint/workspace $externalfshome/$randomint/.jenkins $externalfshome/$randomint/temp $externalfshome/$randomint/.sonar $externalfshome/$randomint/.gradle $externalfshome/$randomint/.npm
-        sudo chown -R 1091:1091 $externalfshome/$randomint
+        USED_PORTS=`netstat -ln | grep ':$randomint ' | grep LISTEN | wc -l`
+
+        while [ $USED_PORTS -gt 0 ]
+        do
+                randomint=`shuf -i 40000-65000 -n1`
+                USED_PORTS=`netstat -ln | grep ':$randomint ' | grep LISTEN | wc -l`
+        done
 }
 
 function create_privileged_container() {
-	sudo docker run --privileged -d -p $randomint:$randomint -v "$externalfshome/$randomint/mavenrepo:/var/data/jenkins/m2/repository" -v "$externalfshome/$randomint/tmp:/tmp" -v "$externalfshome/$randomint/workspace:/var/data/jenkins/workspace" -v "$externalfshome/$randomint/.jenkins:/var/data/jenkins/.jenkins" -v "$externalfshome/$randomint/temp:/var/data/jenkins/temp" -v "$externalfshome/$randomint/.sonar:/var/data/jenkins/.sonar" -v "$externalfshome/$randomint/.gradle:/var/data/jenkins/.gradle" -e master=$master -e executors=1 -e ciuser=fsvctip -e cipassword=sommer11 -e slavename=$imagename-$randomint-`echo $HOSTNAME | cut -d"." -f1` -e labels=$labels -e externalport=$randomint -e host=$HOSTNAME -e additional_args="${additional_args}" --name $imagename-$randomint-$master_hostname ${registry}/${imagename}:${tag} 
+	sudo docker run --privileged -d -p $randomint:$randomint -e master=$master -e executors=1 -e ciuser=fsvctip -e cipassword=sommer11 -e slavename=${imagename:14}-$randomint-`echo $HOSTNAME | cut -d"." -f1` -e labels=$labels -e externalport=$randomint -e host=$HOSTNAME -e additional_args="${additional_args}" --name $imagename-$randomint-$master_hostname ${registry}/${imagename}:${tag} 
 }
 
 function create_container() {
-        sudo docker run -d -p $randomint:$randomint -v "$externalfshome/$randomint/mavenrepo:/var/data/jenkins/m2/repository" -v "$externalfshome/$randomint/tmp:/tmp" -v "$externalfshome/$randomint/workspace:/var/data/jenkins/workspace" -v "$externalfshome/$randomint/.jenkins:/var/data/jenkins/.jenkins" -v "$externalfshome/$randomint/temp:/var/data/jenkins/temp" -v "$externalfshome/$randomint/.sonar:/var/data/jenkins/.sonar" -v "$externalfshome/$randomint/.npm:/var/data/jenkins/.npm" -e master=$master -e executors=1 -e ciuser=fsvctip -e cipassword=sommer11 -e slavename=$imagename-$randomint-`echo $HOSTNAME | cut -d"." -f1` -e labels=$labels -e externalport=$randomint -e host=$HOSTNAME -e additional_args="${additional_args}" --name $imagename-$randomint-$master_hostname ${registry}/${imagename}:${tag}
-    }
+        sudo docker run -d -p $randomint:$randomint -e master=$master -e executors=1 -e ciuser=fsvctip -e cipassword=sommer11 -e slavename=${imagename:14}-$randomint-`echo $HOSTNAME | cut -d"." -f1` -e labels=$labels -e externalport=$randomint -e host=$HOSTNAME -e additional_args="${additional_args}" --name $imagename-$randomint-$master_hostname ${registry}/${imagename}:${tag}
+}
 
 function usage() {
 	echo "You need to provide the following parameters: registry imagename tag master labels"
@@ -51,7 +49,6 @@ then
 fi
 
 check_reserved
-create_directories
 
 if [[ $imagename =~ .*android.* ]]
 then
