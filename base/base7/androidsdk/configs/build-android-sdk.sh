8cbe989abb4d05ef5ae1b22153b024576dbd8f21
u210691 <u210691@sbb.ch>
2015-11-18 13:51:02 +0100

    WZU-4128 Rufe nun die java -version auf, um zu schauen welche Version installiert worden ist und ob diese funktioniert.

--- base/base7/androidsdk/configs/build-android-sdk.sh
+++ base/base7/androidsdk/configs/build-android-sdk.sh
@@ -3,7 +3,7 @@ mkdir -p ${jenkinshome}/buildtools \
 	&& cd ${jenkinshome}/buildtools \
 	&& wget -qO- ${filerurl}/android-sdk.tar.gz | tar xfz - \
 	&& cd ${jenkinshome}/buildtools/android-sdk-linux \
-        && wget ${filerurl}/android-sdk-update.sh -O android-sdk-update.sh \
+        && wget -q ${filerurl}/android-sdk-update.sh -O android-sdk-update.sh \
         && chmod +x ./android-sdk-update.sh \
         && chown -R ${appuser}:${appuser} ${jenkinshome}
 
@@ -17,7 +17,7 @@ tar -zcf /output/android-sdk-linux-latest.tar.gz /output/buildtools
 md5sum /output/android-sdk-linux-latest.tar.gz
 
 # delete file from filerurl
-svn delete --non-interactive --no-auth-cache --username fsbuild --password sommer11 -m "Deleting file android-sdk-linux-latest.tar.gz from build" ${filerurl}/android/android-sdk-linux-latest.tar.gz
+svn delete --non-interactive --no-auth-cache --username fsbuild --password fsbuild2011 -m "Deleting file android-sdk-linux-latest.tar.gz from build" ${filerurl}/android/android-sdk-linux-latest.tar.gz
 
 # upload to wzufiler
-svn import --non-interactive --no-auth-cache --username fsbuild --password sommer11 -m "Updating android-sdk-linux-latest.tar.gz from build" /output/android-sdk-linux-latest.tar.gz ${filerurl}/android/android-sdk-linux-latest.tar.gz
+svn import --non-interactive --no-auth-cache --username fsbuild --password fsbuild2011 -m "Updating android-sdk-linux-latest.tar.gz from build" /output/android-sdk-linux-latest.tar.gz ${filerurl}/android/android-sdk-linux-latest.tar.gz
