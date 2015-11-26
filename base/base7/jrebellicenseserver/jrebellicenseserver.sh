c63a09b1460fb7fa7695b4014000fd480a38ee99
u210691 <u210691@sbb.ch>
2015-11-26 10:35:42 +0100

    WZU-4128 Herunterladen von JDKs angepasst, da Oracle zum Teil unzuverlässig reagiert hat.

--- base/base7/jrebellicenseserver/jrebellicenseserver.sh
+++ base/base7/jrebellicenseserver/jrebellicenseserver.sh
@@ -10,7 +10,6 @@ export datadir=/var/data
 export apphome=/var/data/jrebellicenseserver
 export PATH=/usr/local/bin:/sbin:/usr/sbin:/usr/bin:/bin
 
-
 export JAVA_HOME=/opt/jdk
 
 # Install packages
@@ -27,7 +26,7 @@ echo "export LC_ALL=en_US.UTF-8" > /etc/profile.d/lang.sh
 ln -sf /usr/share/zoneinfo/Europe/Zurich /etc/localtime
 
 # Get and Install Java
-curl -Lks --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u67-b01/jdk-7u67-linux-x64.tar.gz -o /opt/jdk17.tar.gz \
+curl -s --user fsvctip:sommer11 ${jdkurl} -o /opt/jdk17.tar.gz \
 	&& cd /opt && tar xfz jdk17.tar.gz \
 	&& ln -s /opt/jdk1.7* ${JAVA_HOME} \
 	&& rm -rf /opt/*.tar.gz
