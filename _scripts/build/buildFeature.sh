f0c5672d9e3138b01483b40222137a52d2c7efb6
u214892 <u214892@sbb.ch>
2016-01-13 10:07:18 +0100

    SUMO-182

--- _scripts/build/buildFeature.sh
+++ _scripts/build/buildFeature.sh
@@ -84,7 +84,7 @@ do
     image=`basename $path`
     parentimage=`grep "FROM" ${dockerfile} | cut -d/ -f2`
 
-    # If the parent image is built too, then take the tagged image; else take the untagged image
+    # If the parent image is built too, then take the tagged image (which will already be built due to depth-first ordering); else take the untagged image
     if [[ $imagenames =~ $parentimage ]]; then
         echo "For image $image setting parent to  ${REGISTRY}\/$parentimage:${tag}"
 		sed -ri "s#FROM schweizerischebundesbahnen\/$parentimage#FROM ${REGISTRY}\/$parentimage:${tag}#g" ${dockerfile}
