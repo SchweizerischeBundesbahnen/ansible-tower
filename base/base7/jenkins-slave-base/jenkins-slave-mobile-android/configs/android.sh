98bfb458b411ad1d5d26f6ca4c15b4232651e026
u214892 <u214892@sbb.ch>
2016-01-13 15:37:49 +0100

    SUMO-182

---
+++ base/base7/jenkins-slave-base/jenkins-slave-mobile-android/configs/android.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# modifications for some environments
+echo "Set SonarQube instance depending on master"
+case "${master}" in
+	"https://ci.sbb.ch")
+		cat ${jenkinshome}/.codequality/gradle.prod.properties > ${jenkinshome}/.gradle/gradle.properties
+	;;
+    "https://ci-i.sbb.ch/")
+        cat ${jenkinshome}/.codequality/gradle.int.properties > ${jenkinshome}/.gradle/gradle.properties
+	;;
+	"http://ci-t.sbb.ch/")
+        cat ${jenkinshome}/.codequality/gradle.test.properties > ${jenkinshome}/.gradle/gradle.properties
+	;;
+	*)
+	    exit -1
+esac
+cat ${jenkinshome}/.codequality/gradle.properties >> ${jenkinshome}/.gradle/gradle.properties
