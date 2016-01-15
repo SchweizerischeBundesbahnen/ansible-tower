63afe2f75b9dc54caea3af4c5140057cf441e4fa
u214892 <u214892@sbb.ch>
2016-01-15 08:50:36 +0100

    SUMO-160

--- base/base7/jenkins-slave-base/jenkins-slave-mobile-android/configs/android.sh
+++ base/base7/jenkins-slave-base/jenkins-slave-mobile-android/configs/android.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
-
 # modifications for some environments
-echo "Set SonarQube instance depending on master"
+echo "Set SonarQube instance depending on master ${master}"
 case "${master}" in
 	"https://ci.sbb.ch")
 		cat ${jenkinshome}/.codequality/gradle.prod.properties > ${jenkinshome}/.gradle/gradle.properties
