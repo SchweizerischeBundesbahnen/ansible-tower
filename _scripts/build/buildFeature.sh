8988e312c85c2662b519eff3fd7080ca7b60c521
u214892 <u214892@sbb.ch>
2016-02-05 15:25:25 +0100

    WZU-4338 Umstellung auf Registry 2.0

--- _scripts/build/buildFeature.sh
+++ _scripts/build/buildFeature.sh
@@ -87,7 +87,7 @@ do
     # If the parent image is built too, then take the tagged image (which will already be built due to depth-first ordering); else take the untagged image
     # http://stackoverflow.com/questions/8063228/how-do-i-check-if-a-variable-exists-in-a-list-in-bash
     # Since 'base' is also part of 'jenkins-slave-base' etc, regex needs some complexity..., see http://stackoverflow.com/questions/9155590/regexp-match-character-group-or-end-of-line
-    echo $imagenames | grep -q '\(^\|[ ]\)'$parentimage'\($\|[ ]\)')
+    echo $imagenames | grep -q '\(^\|[ ]\)'$parentimage'\($\|[ ]\)'
     is_parent_built=$?
     echo "is_parent_built=${$is_parent_built}"
     if [[ ${is_parent_built} ]]; then
