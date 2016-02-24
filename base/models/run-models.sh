669306db1eb8819238cca344b3ecfdc20183346c
u213900 <pascal.bieri@sbb.ch>
2016-02-24 12:52:55 +0100

    Some more Apache configchanches

--- base/models/run-models.sh
+++ base/models/run-models.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 wget --quiet https://svn.sbb.ch/svn/wzu/models/index.php --user ${fileruser} --password ${filerpassword} --no-check-certificate -O /var/www/models/index.php
 wget --quiet https://svn.sbb.ch/svn/wzu/models/styles.css --user ${fileruser} --password ${filerpassword} --no-check-certificate -O /var/www/models/styles.css
+wget --quiet https://svn.sbb.ch/svn/wzu/models/sbb.png --user ${fileruser} --password ${filerpassword} --no-check-certificate -O /var/www/models/sbb.png
 rm -rf /run/httpd/* /tmp/httpd*
 exec /usr/sbin/apachectl -DFOREGROUND
