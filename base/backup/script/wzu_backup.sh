cca1cf8c491c32676793cda30675042303c11fdb
Elsner Lukas (IT-SWE-MWE-WZU) <lukas.elsner@sbb.ch>
2016-03-24 22:47:11 +0100

    some refactoring, base logic for restore

--- base/backup/script/wzu_backup.sh
+++ base/backup/script/wzu_backup.sh
@@ -4,19 +4,34 @@ cd wzu-backup
 git pull
 source ${CONFIGFILE}
 
-function dumpDatabase {
-        ./dump.sh
+function databaseBackup {
+  ./dump.sh
+}
+
+function databaseRestore {
+  echo "Not implemented yet"
 }
 
 function duplicityBackup {
-        ./duplicity-backup.sh -b -c ${CONFIGFILE}
+  ./duplicity-backup.sh --backup --config ${CONFIGFILE}
 }
 
+function duplicityRestore {
+  ./duplicity-backup.sh --restore /var/data/${NAME}_migrate --config ${CONFIGFILE}
+}
 
-if [ "${TYPE}"  == "DBBACKUP" ] ; then
-        dumpDatabase
-        duplicityBackup
-elif [ "${TYPE}" == "FILEBACKUP" ] ; then
-        duplicityBackup
+if [ "${MODE}" == "BACKUP" ]; then
+  if [ "${TYPE}" == "DB" ]; then
+    databaseBackup
+    duplicityBackup
+  elif [ "${TYPE}" == "FILE" ]; then
+    duplicityBackup
+  fi
+elif [ "${MODE}" == "RESTORE" ]; then
+  if [ "${TYPE}" == "DB" ]; then
+    duplicityRestore
+    databaseRestore
+  elif [ "${TYPE}" == "FILE" ]; then
+    duplicityRestore
+  fi
 fi
-
