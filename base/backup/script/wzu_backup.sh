8e18528bdef9ae153918b6e350cd9bb3e5e5b4c4
Elsner Lukas (IT-SWE-MWE-WZU) <lukas.elsner@sbb.ch>
2016-03-29 11:40:54 +0200

    add some debug messages, defaulting to BACKUP mode

--- base/backup/script/wzu_backup.sh
+++ base/backup/script/wzu_backup.sh
@@ -20,18 +20,33 @@ function duplicityRestore {
   ./duplicity-backup.sh --restore /var/data/${NAME}_migrate --config ${CONFIGFILE}
 }
 
+if [ -z "${TYPE}" ]; then
+  echo "MODE is not set, defaulting to Backup."
+  MODE="BACKUP"
+fi
+
 if [ "${MODE}" == "BACKUP" ]; then
   if [ "${TYPE}" == "DB" ]; then
+    echo "Backing up Database."
     databaseBackup
     duplicityBackup
   elif [ "${TYPE}" == "FILE" ]; then
+    echo "Backing up Filesystem."
     duplicityBackup
+  else
+    echo "Unknown TYPE detected"
   fi
 elif [ "${MODE}" == "RESTORE" ]; then
   if [ "${TYPE}" == "DB" ]; then
+    echo "Restoring Database."
     duplicityRestore
     databaseRestore
   elif [ "${TYPE}" == "FILE" ]; then
+    echo "Restoring Filesystem."
     duplicityRestore
+  else
+    echo "Unknown TYPE detected"
   fi
+else
+  echo "Unknown MODE detected"
 fi
