a777472640d00c0a4da524a018badbbf7baa5dee
Elsner Lukas (IT-SWE-MWE-WZU) <lukas.elsner@sbb.ch>
2016-03-30 13:56:32 +0200

    add migration functionality, more error handling

--- base/backup/configs/wzu_backup.sh
+++ base/backup/configs/wzu_backup.sh
@@ -2,10 +2,12 @@
 
 cd wzu-backup
 git pull
-source ${CONFIGFILE}
+
+CONFIG=${NAME}.conf
+source ${CONFIG}
 
 function databaseBackup {
-  ./dump.sh
+  ./bin/dump.sh
 }
 
 function databaseRestore {
@@ -13,12 +15,29 @@ function databaseRestore {
 }
 
 function duplicityBackup {
-  ./duplicity-backup.sh --backup --config ${CONFIGFILE}
+  ./bin/duplicity-backup.sh --backup --config ${CONFIG}
 }
 
 function duplicityRestore {
-  ./duplicity-backup.sh --restore /var/data/${NAME}_migrate --config ${CONFIGFILE}
+  ./bin/duplicity-backup.sh --restore /var/data/${NAME} --config ${CONFIG}
 }
+
+function migrate() {
+  if [[ -x "migration/${NAME}.sh" ]]
+  then
+    echo "Migration script found. Starting migration."
+    ./migration/${NAME}.sh
+    return $?
+  else
+    echo "File ${NAME}.sh not found or not executable."
+    mv /var/data/${NAME} /var/data/${APPNAME}
+  fi
+}
+
+if [ -z "${NAME}" ]; then
+  echo "Error: NAME is not set, exiting."
+  exit 1
+fi
 
 if [ -z "${MODE}" ]; then
   echo "MODE is not set, defaulting to Backup."
@@ -28,22 +47,28 @@ fi
 if [ "${MODE}" == "BACKUP" ]; then
   if [ "${TYPE}" == "DB" ]; then
     echo "Backing up Database."
-    databaseBackup
-    duplicityBackup
+    databaseBackup \
+    && duplicityBackup
+    exit $?
   elif [ "${TYPE}" == "FILE" ]; then
     echo "Backing up Filesystem."
     duplicityBackup
+    exit $?
   else
     echo "Unknown TYPE: ${TYPE}"
   fi
 elif [ "${MODE}" == "RESTORE" ]; then
   if [ "${TYPE}" == "DB" ]; then
     echo "Restoring Database."
-    duplicityRestore
-    databaseRestore
+    duplicityRestore \
+    && databaseRestore \
+    && migrate
+    exit $?
   elif [ "${TYPE}" == "FILE" ]; then
     echo "Restoring Filesystem."
-    duplicityRestore
+    duplicityRestore \
+    && migrate
+    exit $?
   else
     echo "Unknown TYPE: ${TYPE}"
   fi
