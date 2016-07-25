29a1de4818e226da284703b20237a28101c33584
Elsner Lukas (IT-SWE-MWE-WZU) <lukas.elsner@sbb.ch>
2016-07-25 10:23:05 +0200

    say YES

--- _scripts/jdk-download.sh
+++ _scripts/jdk-download.sh
@@ -115,7 +115,7 @@ function unpack() {
   mkdir -p ${DATADIR} || error
 
   if [ ${FORMAT} == "exe" ]; then
-    7z e -o${TMPDIR} ${TMPDIR}/${FILENAME} > /dev/null 2>&1  || error
+    7z e -y -o${TMPDIR} ${TMPDIR}/${FILENAME} > /dev/null 2>&1  || error
     unzip -q ${TMPDIR}/tools.zip -d ${DATADIR} || error
     rm ${TMPDIR}/tools.zip
     for file in $(find "${DATADIR}" -name "*pack"); do ${JAVA_HOME}/bin/unpack200 -r "${file}" "${file/%pack/jar}"; done
