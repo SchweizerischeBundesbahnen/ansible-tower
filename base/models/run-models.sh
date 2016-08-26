08cae5f58e706503d26f9266db6b74803bc2d9ee
u217229 <sebastian.graf@sbb.ch>
2016-08-26 15:27:43 +0200

    CDP-121: removing invalid images

--- base/models/run-models.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-wget --quiet https://svn.sbb.ch/svn/wzu/models/index.php --user ${fileruser} --password ${filerpassword} --no-check-certificate -O /var/www/models/index.php
-wget --quiet https://svn.sbb.ch/svn/wzu/models/styles.css --user ${fileruser} --password ${filerpassword} --no-check-certificate -O /var/www/models/styles.css
-wget --quiet https://svn.sbb.ch/svn/wzu/models/sbb.png --user ${fileruser} --password ${filerpassword} --no-check-certificate -O /var/www/models/sbb.png
-rm -rf /run/httpd/* /tmp/httpd*
-chown -R apache:apache /var/www/models
-exec /usr/sbin/apachectl -DFOREGROUND
