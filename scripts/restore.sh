a9f4c7c88410c2e9d5b74bac931fb04241681f36
u217229 <sebastian.graf@sbb.ch>
2016-09-14 11:42:41 +0200

    CDP-73: restore finished

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
-if [ "! $(ls -A /backup/tower-backup-latest.tar.gz))" ] ; then
+if [ ! -f /backup/tower-backup-latest.tar.gz ] ; then
     echo "/backup/tower-backup-latest.tar.gz not existing, no backup available"
     exit 102
 fi
 
+rm /var/lib/postgresql/9.4/main/.gitignore /var/lib/awx/.gitignore
 if [ "$(ls -A /var/lib/postgresql/9.4/main)" ] || [ "$(ls -A /var/lib/awx)" ]; then
     echo "DB (/var/lib/postgresql/9.4/main) and/or Data (/var/lib/awx) existing. Remove on Host first and try again. Exiting..."
     exit 102
 fi
 
-#Starting Backup
-/opt/tower-setup/setup.sh -r /backup/tower-backup-latest.tar.gz
+#Copying backup to correct location
+cp /backup/tower-backup-latest.tar.gz /opt/tower-setup/tower-backup-latest.tar.gz
+
+#Bootstrapping first and starting and waiting
+./docker-entrypoint.sh initialize
+ansible-tower-service start
+sleep 10
+#restoring
+/opt/tower-setup/setup.sh -r /opt/tower-setup/tower-backup-latest.tar.gz
+sleep 10
+ansible-tower-service stop
